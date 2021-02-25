fa14debc13287eb622da99318da3e806ade3f12f
Seda Özdemir <seda.ozdemir8@ogrenci.selcuk.edu.tr>
2021-02-25 16:10:44 +0300

    Proje dosyası ekle.

---
+++ CampIntro2/MyDictionary.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CampIntro2
+{
+    class MyDictionary<K,V>
+    {
+        K[] keys;
+        K[] _tempArray;
+        V[] values;
+        V[] _tempArray2;
+        
+
+        public MyDictionary()
+        {
+            keys = new K[0];
+            values = new V[0];
+        }
+
+        public void Add(K KeyItem , V ValueItem)
+        {
+            //Anahtarın eklenmesi
+            _tempArray = keys;
+            keys = new  K[keys.Length + 1];
+            for (int i = 0; i < _tempArray.Length; i++)
+            {
+                keys[i] = _tempArray[i];
+
+            }
+            keys[keys.Length - 1] = KeyItem;
+
+            //Değerin Eklenmesi
+            _tempArray2 = values;
+            values = new V[values.Length + 1];
+            for (int i = 0; i < _tempArray2.Length; i++)
+            {
+                values[i] = _tempArray2[i];
+
+             }
+            values[values.Length - 1] = ValueItem;
+        }
+        private int _count;
+
+        public int Count
+        {
+            get { return keys.Length; }
+
+        }
+    }
+}
---
+++ CampIntro2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CampIntro2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> products = new MyDictionary<int, string>();
+            products.Add(52364892, "Mavi Jean");
+            products.Add(59518415, "Siyah Kaban");
+            products.Add(52974625, "Ekru Jean");
+            products.Add(59614854, "Bordo Kaban");
+            Console.WriteLine(products.Count);
+
+
+        }
+    }
+}
